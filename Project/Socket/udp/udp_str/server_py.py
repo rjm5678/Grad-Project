c05fd327aca3b56e69696612bd8b544aedce766e
rjm5678 <799153572@qq.com>
2019-04-17 20:43:08 +0800

    modified udp_str

--- Project/Socket/udp/udp_str/server_py.py
+++ Project/Socket/udp/udp_str/server_py.py
@@ -1,10 +1,12 @@
+#!/usr/bin/python3
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("47.102.216.81", 9090))
+sock.bind(("", 9090))
 
 while True:
     message, addr = sock.recvfrom(1024)
-    print("Received from client: {}".format(message))
-
+    print("Received from {}: {}".format(addr, message))
     sock.sendto(bytes(message), addr)
+    if message == b'q':
+         break
